5b253d513aa0027db4fe69c3a7e05542098ed215
Danik <daniknewgarden@gmail.com>
2021-01-19 22:16:11 +0200

    Added ListCard component

---
+++ src/Components/ListCard.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+//Styled components
+import styled from "styled-components";
+
+type ListCardProps = {
+  title: string;
+  listItems: Array<React.ReactElement>;
+  className?: string;
+};
+
+const ListCard: React.FC<ListCardProps> = ({ title, className, listItems }) => {
+  return (
+    <div className={className}>
+      <h2>{title}</h2>
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
+
+const StyledListCard = styled(ListCard)`
+  --color-text: #000000;
+  --color-item: #555770;
+  --color-border: #e3e4eb;
+  --size-radius: 16px;
+
+  text-align: left;
+  padding: 56px 40px;
+  border: 2px solid var(--color-border);
+  border-radius: var(--size-radius);
+
+  h2 {
+    margin: 0;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 36px;
+    color: var(--color-text);
+  }
+
+  ul {
+    padding: 0;
+    margin-left: 18px;
+  }
+
+  li {
+    text-align: left;
+    font-size: 16px;
+    line-height: 150%;
+    color: var(--color-item);
+  }
+`;
+
+export default StyledListCard;
