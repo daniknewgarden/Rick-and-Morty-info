f7b9110e57175106677ad4e30410856f68f92bcd
Danik <daniknewgarden@gmail.com>
2021-01-19 12:34:53 +0200

    Added characted info request to info page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Route, useHistory } from "react-router-dom";
 //Pages
 import HomePage from "./Pages/Home";
 import InfoPage from "./Pages/Info";
-
 function App() {
   const history = useHistory();
 
@@ -14,13 +13,18 @@ function App() {
     history.push("/home");
   }, []);
 
+  //States
+  const [characterId, setCharacterId] = useState<string | null>(null);
+  const [loadImage, setLoadImage] = useState<boolean>(false);
+  const [loadEpisode, setLoadEpisode] = useState<boolean>(false);
+
   return (
     <div className="App">
       <Route path="/home">
         <HomePage />
       </Route>
       <Route path="/info">
-        <InfoPage />
+        <InfoPage id="1" loadImage={loadImage} loadEpisode={loadEpisode} />
       </Route>
     </div>
   );
--- src/Pages/Info.tsx
+++ src/Pages/Info.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 //Routing
 import { useHistory } from "react-router-dom";
+//Components
 import Button from "../Components/Button";
 import Title from "../Components/Title";
+//GraphQL
+import { useQuery } from "@apollo/client";
+import GET_CHARACTER_INFO from "../GraphQL/getCharacterInfo";
 
-const InfoPage: React.FC = () => {
+type InfoPageTypes = {
+  id: String;
+  loadImage: Boolean;
+  loadEpisode: Boolean;
+};
+
+const InfoPage: React.FC<InfoPageTypes> = ({
+  id = "1",
+  loadImage,
+  loadEpisode,
+}) => {
   const history = useHistory();
+
+  const { data, loading, error } = useQuery(GET_CHARACTER_INFO, {
+    variables: { id: id, image: loadImage, episode: loadEpisode },
+  });
 
   return (
     <div>
-      <Title text="Info page" />
+      <Title text={data ? data.character.name : loading ? "Loading" : error} />
       <Button
         callBack={() => history.push("/home")}
         text="Back to choose"
