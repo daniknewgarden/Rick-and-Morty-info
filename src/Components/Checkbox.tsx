87565178ea14a261f902c514977d016f2f74d1d9
Danik <daniknewgarden@gmail.com>
2021-01-20 17:57:10 +0200

    Checkbox component: added customization

--- src/Components/Checkbox.tsx
+++ src/Components/Checkbox.tsx
@@ -7,6 +7,14 @@ type CheckBoxProps = {
   name: string;
   text: string;
   className?: string;
+  //Default states
+  disabled?: boolean;
+  defaultChecked?: true;
+  // Custom styles
+  colorDefault?: string;
+  colorHover?: string;
+  colorChecked?: string;
+  colorDisabled?: string;
 };
 
 const Checkbox: React.FunctionComponent<CheckBoxProps> = ({
@@ -14,6 +22,8 @@ const Checkbox: React.FunctionComponent<CheckBoxProps> = ({
   text,
   onCheck,
   className,
+  defaultChecked,
+  disabled,
 }) => (
   <label htmlFor={name} className={className}>
     <input
@@ -22,6 +32,8 @@ const Checkbox: React.FunctionComponent<CheckBoxProps> = ({
       id={name}
       onChange={onCheck}
       value={text}
+      checked={defaultChecked}
+      disabled={disabled}
     />
     {text}
   </label>
@@ -38,21 +50,6 @@ const StyledCheckbox = styled(Checkbox)`
   line-height: 150%;
 
   input {
-    --color-accent: #555770;
-
-    &:hover {
-      --color-accent: #6690ff;
-    }
-
-    &:focus,
-    &:checked {
-      --color-accent: #254eda;
-    }
-
-    &:disabled {
-      --color-accent: #e3e4eb;
-    }
-
     margin: 0;
     width: 16px;
     height: 16px;
@@ -65,19 +62,42 @@ const StyledCheckbox = styled(Checkbox)`
       height: 100%;
 
       border-radius: 2px;
-      box-shadow: 0 0 0 2px var(--color-accent);
+    }
+
+    --color-main: ${({ colorDefault = "#555770" }) => colorDefault};
+
+    &::before {
+      box-shadow: 0 0 0 2px var(--color-main);
+    }
+
+    &:hover {
+      --color-main: ${({ colorHover = "#6690ff" }) => colorHover};
     }
 
     &:focus {
       outline: none;
+      --color-main: ${({ colorChecked = "#254eda" }) => colorChecked};
+      &:checked {
+        --color-main: ${({ colorHover = "#6690ff" }) => colorHover};
+      }
+    }
+
+    &:disabled {
+      --color-main: ${({ colorDisabled = "#e3e4eb" }) => colorDisabled};
+
+      &:hover,
+      &:checked {
+        --color-main: ${({ colorDisabled = "#e3e4eb" }) => colorDisabled};
+      }
     }
 
     &:checked {
+      --color-main: ${({ colorChecked = "#254eda" }) => colorChecked};
       &::before {
         background-image: url("data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg clip-path='url(%23clip0)'%3E%3Cpath d='M3.82918 8.73562C3.73372 8.83164 3.60349 8.88521 3.46821 8.88521C3.33292 8.88521 3.20269 8.83164 3.10723 8.73562L0.224382 5.85231C-0.0747942 5.55313 -0.0747942 5.068 0.224382 4.76938L0.585358 4.40831C0.884628 4.10914 1.3692 4.10914 1.66838 4.40831L3.46821 6.20823L8.3316 1.34474C8.63087 1.04557 9.11591 1.04557 9.41462 1.34474L9.7756 1.70581C10.0748 2.00499 10.0748 2.49003 9.7756 2.78874L3.82918 8.73562Z' fill='white'/%3E%3C/g%3E%3Cdefs%3E%3CclipPath id='clip0'%3E%3Crect width='10' height='10' fill='white'/%3E%3C/clipPath%3E%3C/defs%3E%3C/svg%3E");
         background-repeat: no-repeat;
         background-position: center;
-        background-color: var(--color-accent);
+        background-color: var(--color-main);
       }
     }
   }
