030c7a244ccbf6d913625df42e91411548332830
Danik <daniknewgarden@gmail.com>
2021-01-21 23:10:06 +0200

    Button component: added customization

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,24 +2,38 @@ import React from "react";
 //styled components
 import styled, { css } from "styled-components";
 
-type ButtonProps = {
+export type ButtonProps = {
   callBack: (event: React.MouseEvent<HTMLButtonElement>) => void;
   text?: string;
   ariaLabel?: string;
   // Custom styles
   className?: string;
-  size?: "big" | "small";
+  big?: boolean;
   rounded?: boolean;
   transparent?: boolean;
+  disabled?: boolean;
+  // Colors
+  colorText?: string;
+  colorMain?: string;
+  colorHover?: string;
+  colorFocus?: string;
+  colorOutline?: string;
+  colorDisabled?: string;
 };
 
 const Button: React.FC<ButtonProps> = ({
   text,
   ariaLabel,
   className,
+  disabled,
   callBack,
 }) => (
-  <button onClick={callBack} className={className} aria-label={ariaLabel}>
+  <button
+    onClick={callBack}
+    className={className}
+    aria-label={ariaLabel}
+    disabled={disabled}
+  >
     {text}
   </button>
 );
@@ -32,7 +46,7 @@ const sizes = {
 
     padding: 12px 24px;
   `,
-  small: css`
+  default: css`
     font-size: 14px;
     line-height: 22px;
 
@@ -52,43 +66,51 @@ const corners = {
 const background = {
   transparent: css`
     background: transparent;
-    color: var(--color-accent);
-    border: 2px solid var(--color-accent);
+    color: var(--color);
+    border: 2px solid var(--color);
+
+    &:focus {
+      --color: var(--color-hover);
+    }
   `,
   filed: css`
-    background: var(--color-accent);
+    background: var(--color);
     color: var(--color-text);
     border: none;
 
     &:focus {
-      box-shadow: 0 0 0 2px var(--color-border);
+      box-shadow: 0 0 0 2px var(--color-outline);
     }
   `,
 };
 
-const StyledButton = styled(Button)`
-  --color-accent: #3366ff;
-  --color-border: #6690ff;
+// ${({  = '' }) => }
 
-  --color-text: #fff;
+const StyledButton = styled(Button)`
+  --color-main: ${({ colorMain = "#3366ff" }) => colorMain};
+  --color-hover: ${({ colorHover = "#6690FF" }) => colorHover};
+  --color-focus: ${({ colorFocus = "#254EDA" }) => colorFocus};
+  --color-outline: ${({ colorOutline = "#6690FF" }) => colorOutline};
+  --color-disabled: ${({ colorDisabled = "#254eda" }) => colorDisabled};
+  --color-text: ${({ colorText = "#fff" }) => colorText};
+
+  --color: var(--color-main);
+
+    &:hover {
+      --color: var(--color-hover);
+    }
+    &:focus,
+    &:active {
+      outline: none;
+      --color: var(--color-focus);
+    }
+    &:disabled {
+      --color: var(--color-disabled);
+    };
 
   font-weight: 500;
 
-  &:hover {
-    --color-accent: #6690ff;
-  }
-
-  &:active,
-  &:focus {
-    --color-accent: #254eda;
-    outline: none;
-  }
-
-  &:disabled {
-    --color-accent: #e3e4eb;
-  }
-
-  ${({ size = "small" }) => sizes[size]}
+  ${({ big = false }) => (big ? sizes["big"] : sizes["default"])}
 
   ${({ rounded = false }) =>
     rounded ? corners["rounded"] : corners["default"]}
